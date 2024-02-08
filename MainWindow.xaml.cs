c7c0e77cfc1f75bf547fcd7a1e1fc712a047996e
Masterlevwin <114339933+Masterlevwin@users.noreply.github.com>
2024-02-08 19:20:02 +0300

    ok

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Metal_Code
         public void OnPropertyChanged([CallerMemberName] string prop = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
 
         public static MainWindow M = new();
-        readonly string version = "2.0.0";
+        readonly string version = "2.0.5";
 
         //public readonly TypeDetailContext dbTypeDetails = new();
         //public readonly WorkContext dbWorks = new();
