9bc4df0cba36262548ffe87495b18c86b350e10d
Михаил Серых <masterlevwin@mail.ru>
2024-07-05 15:04:57 +0300

    Добавлен блок строк подключения к базам для тестирования на рабочем месте.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,15 +39,27 @@ namespace Metal_Code
         public readonly string[] connections =
         {
             "Data Source=managers.db",
-            $"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\managers.db",
+            $"Data Source = C:\\Users\\User\\source\\repos\\Masterlevwin\\Metal-Code\\ver.2.4.3_Восстановить базы\\managers.db",
             "Data Source=typedetails.db",
-            $"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\typedetails.db",
+            $"Data Source = C:\\Users\\User\\source\\repos\\Masterlevwin\\Metal-Code\\ver.2.4.3_Восстановить базы\\typedetails.db",
             "Data Source=works.db",
-            $"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\works.db",
+            $"Data Source = C:\\Users\\User\\source\\repos\\Masterlevwin\\Metal-Code\\ver.2.4.3_Восстановить базы\\works.db",
             "Data Source=metals.db",
-            $"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\metals.db",
-            $"C:\\Users\\maste\\Metal-Code\\bin\\Release\\net7.0-windows",
-            $"C:\\Users\\maste\\Metal-Code\\bin\\Release\\net7.0-windows"
+            $"Data Source = C:\\Users\\User\\source\\repos\\Masterlevwin\\Metal-Code\\ver.2.4.3_Восстановить базы\\metals.db",
+            $"C:\\Users\\User\\source\\repos\\Masterlevwin\\Metal-Code\\bin\\Release\\net7.0-windows",
+            $"C:\\Users\\User\\source\\repos\\Masterlevwin\\Metal-Code\\bin\\Release\\net7.0-windows"
+
+            //"Data Source=managers.db",
+            //$"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\managers.db",
+            //"Data Source=typedetails.db",
+            //$"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\typedetails.db",
+            //"Data Source=works.db",
+            //$"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\works.db",
+            //"Data Source=metals.db",
+            //$"Data Source = C:\\Users\\maste\\Metal-Code\\ver.2.4.3_Восстановить базы\\metals.db",
+            //$"C:\\Users\\maste\\Metal-Code\\bin\\Release\\net7.0-windows",
+            //$"C:\\Users\\maste\\Metal-Code\\bin\\Release\\net7.0-windows"
+
             //"Data Source=managers.db",
             //$"Data Source = Y:\\Конструкторский отдел\\Расчет Заказов ЛФ Сервер\\Metal-Code\\managers.db",
             //"Data Source=typedetails.db",
@@ -777,7 +789,7 @@ namespace Metal_Code
                             _man?.Offers.Add(_offer);       //переносим расчет в базу этого менеджера
                         }
 
-                    db.SaveChanges();                       //сохраняем изменения в основной базе данных
+                    db.SaveChangesAsync();                  //сохраняем изменения в основной базе данных
                     StatusBegin($"Основная база обновлена. Сейчас в базе {db.Offers.ToList().Count} расчетов.");
 
                     //очищаем списки во временном словаре
