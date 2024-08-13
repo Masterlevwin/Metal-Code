53f54688488eea65a9eb35f176bfb3f8f0065537
Михаил Серых <masterlevwin@mail.ru>
2024-08-13 10:43:29 +0300

    Убран задублированный коэффициент за массу детали из алгоритма расчета.

--- BendControl.xaml.cs
+++ BendControl.xaml.cs
@@ -263,7 +263,7 @@ namespace Metal_Code
             };
 
             return BendDict.ContainsKey(work.type.S) ?
-                _bendRatio * _count * BendDict[work.type.S][$"{ShelfDrop.SelectedItem}"] * MainWindow.MassRatio(_mass) * MainWindow.MassRatio(_mass) : 0;
+                _bendRatio * _count * BendDict[work.type.S][$"{ShelfDrop.SelectedItem}"] * MainWindow.MassRatio(_mass) : 0;
         }
 
         public void SaveOrLoadProperties(UserControl uc, bool isSaved)
