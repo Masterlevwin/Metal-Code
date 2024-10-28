74e996d76a99ef5586b031f38fd6eed7f4cfe9a3
Михаил <newmasterlevwin@gmail.com>
2024-10-28 23:10:19 +0300

    Create Tech.cs

---
+++ Tech.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Tech
+{
+public string Path;
+
+public Tech(string path)
+{
+Path = path;
+}
+
+public string Run()
+{
+string notify = $"Не удается прочитать файл";
+
+
+
+return notify;
+}
+}
