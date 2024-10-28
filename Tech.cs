48c1c31ddd0cd2314a1ba3a7b5072fad75d7b241
Михаил <newmasterlevwin@gmail.com>
2024-10-28 23:38:58 +0300

    Update Tech.cs

--- Tech.cs
+++ Tech.cs
@@ -1,4 +1,7 @@
 using System;
+using System.IO;
+using System.Collections;
+using ExcelDataReader;
 
 public class Tech
 {
@@ -12,8 +15,22 @@ Path = path;
 public string Run()
 {
 string notify = $"Не удается прочитать файл";
+  
+  using FileStream stream = File.Open(Path, FileMode.Open, FileAccess.Read);              
+  using IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);             
+  DataSet result = reader.AsDataSet();              
+  DataTable table = result.Tables[0];
 
+  List<string> names = new();
+  for (int i = 1; i < table.Rows.Count; i++)
+  {
+    names.Add($"{table.Rows[i].ItemArray[1]} "
+              + "{table.Rows[i].ItemArray[2]} " 
+              + "s{table.Rows[i].ItemArray[3]} "
+              + "n{table.Rows[i].ItemArray[4]}");
+  }
 
+  DirectoryInfo dirLaser = Directory.CreateDirectory(Path.GetDirectoryName(Path));
 
 return notify;
 }
