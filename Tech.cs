4b1968a106377f11a408e7f0c23a491ea69a413d
Михаил <newmasterlevwin@gmail.com>
2024-10-29 10:09:56 +0300

    Update Tech.cs

--- Tech.cs
+++ Tech.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using Path = System.IO.Path;
 using System.Collections.Generic;
+using System.Linq;
 using ExcelDataReader;
 
 public class Tech
@@ -23,18 +24,26 @@ string notify = $"Не удается прочитать файл";
   DataTable table = result.Tables[0];
 
   List<string> names = new();
+  List<string> materials = new();
   for (int i = 1; i < table.Rows.Count; i++)
   {
     names.Add($"{table.Rows[i].ItemArray[1]} "
               + "{table.Rows[i].ItemArray[2]} " 
               + "s{table.Rows[i].ItemArray[3]} "
               + "n{table.Rows[i].ItemArray[4]}");
+    materials.Add($"s{table.Rows[i].ItemArray[3]} "
+              + "{table.Rows[i].ItemArray[2]}");
   }
 
   string[] files = Directory.GetFiles(Path.GetDirectoryName(ExcelFile));
   
   DirectoryInfo dirLaser = Directory.CreateDirectory(
     Path.GetDirectoryName(ExcelFile) + "\\" + "Лазер");
+  
+  var dirMaterials = materials.GroupBy();
+  foreach (var item in dirMaterials)
+    Directory.CreateDirectory(
+    Path.GetDirectoryName(dirLaser) + "\\" + $"{item}");
   
   foreach (string name in names)
     foreach (string file in files)
