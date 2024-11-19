04a5c0dea936f778c10d384f461340eaa7e68135
Михаил Серых <masterlevwin@mail.ru>
2024-11-19 10:04:55 +0300

    Исправлена ошибка в методе проверки версии.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4288,7 +4288,7 @@ namespace Metal_Code
 
         public bool CheckVersion(out string _version)                   //метод проверки версии приложения
         {
-            if (IsLocal || !File.Exists(connections[9] + "\\version.txt"))
+            if (!IsLocal || !File.Exists(connections[9] + "\\version.txt"))
             {
                 _version = $"{Version}, без подключения к серверу.";
                 return true;
