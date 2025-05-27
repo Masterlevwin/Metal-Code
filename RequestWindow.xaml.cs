dc5d2b62b152194529fa4265fb82f4c8bd3a78e9
Михаил Серых <masterlevwin@mail.ru>
2025-05-27 10:05:57 +0300

    Добавлено окно для настройки заявки клиента.

---
+++ RequestWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Metal_Code
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWindow.xaml
+    /// </summary>
+    public partial class RequestWindow : Window
+    {
+        public RequestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
