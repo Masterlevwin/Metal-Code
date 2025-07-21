a6cef591c915990058f8bf315411df9902e7a947
Михаил Серых <masterlevwin@mail.ru>
2025-07-21 17:56:21 +0300

    Добавлены оси координат для отладки.

--- ArcDescriptor.cs
+++ ArcDescriptor.cs
@@ -68,6 +68,105 @@ namespace Metal_Code
 
             // Добавляем на Canvas
             canvas.Children.Add(path);
+
+            DrawCoordinateSystem(canvas, length: 50, thickness: 0.5, offsetX: 60, offsetY: 60);
+        }
+        public static void DrawCoordinateSystem(Canvas canvas, double length = 100, double thickness = 0.5, double offsetX = 0, double offsetY = 0)
+        {
+            // Ось X (красная)
+            var xAxis = new Line
+            {
+                X1 = 0,
+                Y1 = 0,
+                X2 = length,
+                Y2 = 0,
+                Stroke = Brushes.Red,
+                StrokeThickness = thickness
+            };
+
+            // Стрелка на оси X
+            var xArrow = new Line
+            {
+                X1 = length,
+                Y1 = 0,
+                X2 = length - 5,
+                Y2 = -2,
+                Stroke = Brushes.Red,
+                StrokeThickness = thickness
+            };
+
+            var xArrow2 = new Line
+            {
+                X1 = length,
+                Y1 = 0,
+                X2 = length - 5,
+                Y2 = 2,
+                Stroke = Brushes.Red,
+                StrokeThickness = thickness
+            };
+
+            // Ось Y (синяя)
+            var yAxis = new Line
+            {
+                X1 = 0,
+                Y1 = 0,
+                X2 = 0,
+                Y2 = length,
+                Stroke = Brushes.Blue,
+                StrokeThickness = thickness
+            };
+
+            // Стрелка на оси Y
+            var yArrow = new Line
+            {
+                X1 = 0,
+                Y1 = length,
+                X2 = 2,
+                Y2 = length - 5,
+                Stroke = Brushes.Blue,
+                StrokeThickness = thickness
+            };
+
+            var yArrow2 = new Line
+            {
+                X1 = 0,
+                Y1 = length,
+                X2 = -2,
+                Y2 = length - 5,
+                Stroke = Brushes.Blue,
+                StrokeThickness = thickness
+            };
+
+            // Текстовые обозначения осей
+            var xAxisText = new TextBlock
+            {
+                Text = "X",
+                Foreground = Brushes.Red,
+                FontSize = 6
+            };
+
+            var yAxisText = new TextBlock
+            {
+                Text = "Y",
+                Foreground = Brushes.Blue,
+                FontSize = 6
+            };
+
+            Canvas.SetLeft(xAxisText, length + 2);
+            Canvas.SetTop(xAxisText, 2);
+
+            Canvas.SetLeft(yAxisText, 2);
+            Canvas.SetTop(yAxisText, length - 8);
+
+            // Добавляем на Canvas
+            canvas.Children.Add(xAxis);
+            canvas.Children.Add(xArrow);
+            canvas.Children.Add(xArrow2);
+            canvas.Children.Add(yAxis);
+            canvas.Children.Add(yArrow);
+            canvas.Children.Add(yArrow2);
+            canvas.Children.Add(xAxisText);
+            canvas.Children.Add(yAxisText);
         }
     }
 
