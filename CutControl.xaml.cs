6053c43dd26b2d7f0c92fd3354193ffc25fa8fcf
Михаил Серых <masterlevwin@mail.ru>
2025-11-17 16:10:28 +0300

    Поправлено предупреждение о кол-ве символов в наименованиях деталей.

--- CutControl.xaml.cs
+++ CutControl.xaml.cs
@@ -349,8 +349,8 @@ namespace Metal_Code
 
             if (title != null && title.Length > 30)
             {
-                if (MainWindow.M.Log is null || !MainWindow.M.Log.Contains($"Проверьте наименования деталей в Лазере!\nОни не должны быть длиннее 30 символов."))
-                    MainWindow.M.Log += $"\nПроверьте наименования деталей в Лазере!\nОни не должны быть длиннее 30 символов.\n";
+                if (MainWindow.M.Log is null || !MainWindow.M.Log.Contains($"Проверьте наименования деталей в Лазере!\nОни не должны быть длиннее 40 символов."))
+                    MainWindow.M.Log += $"\nПроверьте наименования деталей в Лазере!\nОни не должны быть длиннее 40 символов.\n";
             }
 
             if (MainWindow.M.TechItems.Count > 0)
